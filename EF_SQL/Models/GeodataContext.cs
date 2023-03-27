87588fcbe66a4ebe9b6891064d7cf650153c0a17
Travis Butcher <tbutcher@esri.com>
2023-03-27 17:43:14 +0000

    Update Git Structure

---
+++ EF_SQL/Models/GeodataContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_SQL.Models;
+
+public partial class GeodataContext : DbContext
+{
+    public GeodataContext()
+    {
+    }
+
+    public GeodataContext(DbContextOptions<GeodataContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Parcel> Parcels { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=geodata;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Parcel>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.Other)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .HasColumnName("other");
+            entity.Property(e => e.Parcelid).HasColumnName("parcelid");
+            entity.Property(e => e.Parcelname)
+                .HasMaxLength(50)
+                .HasColumnName("parcelname");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EF_SQL/Models/Parcel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF_SQL.Models;
+
+public partial class Parcel
+{
+    public Guid? Parcelid { get; set; }
+
+    public string? Parcelname { get; set; }
+
+    public string? Other { get; set; }
+}
